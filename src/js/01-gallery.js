15271f81025728a04d16b44e46e51660644ffac6
Serhii_Rud <carmagsales@gmail.com>
2023-02-21 16:38:55 +0200

    1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryElem = document.querySelector('.gallery');
+const galleryMarkup = galleryItems
+  .map(
+    ({
+      preview,
+      original,
+      description,
+    }) => `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+  )
+  .join('');
+galleryElem.insertAdjacentHTML('beforeend', galleryMarkup);
 
-console.log(galleryItems);
+const lightbox = new SimpleLightbox('.gallery a', {
+  overlayOpacity: 0.6,
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+  animationSpeed: 500,
+});
