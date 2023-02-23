af31e441283cbc323269523a397fed30542abeab
Serhii_Rud <carmagsales@gmail.com>
2023-02-23 20:55:06 +0200

    ready

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const STORAGE_KEY = 'videoplayer-current-time';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const onPlay = function ({ duration, percent, seconds }) {
+  localStorage.setItem(STORAGE_KEY, seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+try {
+  player.setCurrentTime(JSON.parse(localStorage.getItem(STORAGE_KEY)) || 0);
+} catch (error) {
+  console.log(error.message);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,52 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
+
+restoreInput();
+
+function saveInput(event) {
+  event.preventDefault();
+
+  const { email, message } = formEl.elements;
+  localStorage.setItem(
+    'feedback-form-state',
+    JSON.stringify({
+      email: email.value,
+      message: message.value,
+    })
+  );
+  restoreInput();
+}
+
+function restoreInput() {
+  try {
+    const restoredInput = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    if (restoredInput) {
+      formEl.email.value = restoredInput.email;
+      formEl.message.value = restoredInput.message;
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+function onSubmit(event) {
+  if (
+    event.currentTarget.email.value === '' ||
+    event.currentTarget.message.value === ''
+  ) {
+    return alert(`Please fill all fields!`);
+  }
+  event.preventDefault();
+  const messageObject = {
+    email: formEl.email.value,
+    message: formEl.message.value,
+  };
+  console.log(messageObject);
+  event.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+formEl.addEventListener('input', throttle(saveInput, 500));
+formEl.addEventListener('submit', onSubmit);
